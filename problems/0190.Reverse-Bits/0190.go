496caae7bad9964be8127ae2b3bc06b3b377fefe
Nhat Pham <nhatpham218@gmail.com>
2026-02-16 09:28:02 +0700

    Refactor reverseBits function in 0190.go to use int type instead of uint32 for input and output.

--- problems/0190.Reverse-Bits/0190.go
+++ problems/0190.Reverse-Bits/0190.go
@@ -2,8 +2,8 @@ package leetcode
 
 // 190. Reverse Bits
 // https://leetcode.com/problems/reverse-bits/description/
-func reverseBits(n uint32) uint32 {
-	reversed := uint32(0)
+func reverseBits(n int) int {
+	reversed := int(0)
 	for i := 0; i < 32; i++ {
 		reversed <<= 1
 		reversed |= n & 1
